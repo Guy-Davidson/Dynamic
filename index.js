ee0c687eb73919a96f2a5f886acbcb37244f9f16
Guy Davidson <guydav12@gmail.com>
2022-05-23 21:38:25 +0300

    reading ips

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const fs = require('fs')
 const express = require('express');
 const app = express();
 
+const ips = fs.readFileSync('ips.txt','utf8')
+
 app.get('/', (req, res) => {    
-    res.send(`<h1>You Are Truly the Greatest!</h1>`)
+    res.send(`<h1>You Are Truly the Greatest! ${ips}</h1>`)
 })
 
 const PORT = process.env.PORT || 5000
