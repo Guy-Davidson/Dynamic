bfc1e087c9b4932cf0a0d90d9b13870909edd66c
Guy Davidson <guydav12@gmail.com>
2022-05-23 20:45:25 +0300

    truly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {    
-    res.send("<h1>You Are the Greatest!</h1>")
+    res.send(`<h1>You Are Truly the Greatest!</h1>`)
 })
 
 const PORT = process.env.PORT || 5000
