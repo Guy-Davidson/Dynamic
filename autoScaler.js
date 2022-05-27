0cad5655c8d222c1e9d7ad683e355e9bb1d75940
Guy Davidson <guydav12@gmail.com>
2022-05-27 09:34:46 +0300

    exce cmd change

--- autoScaler.js
+++ autoScaler.js
@@ -43,8 +43,8 @@ const initAutoScaler = async (queue) => {
                                     if (err) console.log(err, err.stack)
                                     else {                                        
                                         let newWorkerIP = data["Reservations"][0]["Instances"][0]["PublicIpAddress"]
-
-                                        exec(`initWorker.bash ${newWorkerIP}`, { 'shell': true }, (err, stdout, stderr)=> {
+                                        
+                                        exec(`sudo bash ~/app/initWorker.bash ${newWorkerIP}`, { 'shell': true }, (err, stdout, stderr)=> {
                                             if(err) console.log(err);
                                             else {
                                                 console.log(stdout);
