d67bc341c06df7622b815cee83fdfba74caf910c
Guy Davidson <guydav12@gmail.com>
2022-05-27 10:57:19 +0300

    initWorker.bash changes

--- initWorker.bash
+++ initWorker.bash
@@ -1,5 +1,5 @@
-$WORKER_IP=$1 
-$KEY_PEM=$(find ../ -name 'Cloud-Computing-*')
+WORKER_IP=$1 
+KEY_PEM=$(find ../ -name 'Cloud-Computing-*')
 
 scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ../ips.txt ubuntu@${WORKER_IP}:/home/ubuntu/
 scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" onWorkerScript.bash ubuntu@${WORKER_IP}:/home/ubuntu/
