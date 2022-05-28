28ca402b0621194195e6f15b56f8e59b33867c23
Guy Davidson <guydav12@gmail.com>
2022-05-28 12:15:38 +0300

    permission r w

--- lunchWorker.js
+++ lunchWorker.js
@@ -12,7 +12,7 @@ const lunchWorker = () => {
             let keyname = keyData["KeyName"]
             let keyMaterial = keyData["KeyMaterial"]
             fs.writeFileSync(`${keyname}.pem`, keyMaterial);
-            fs.chmodSync(`${keyname}.pem`, 400)
+            fs.chmodSync(`${keyname}.pem`, fs.constants.S_IRUSR | fs.constants.S_IWUSR)
 
             exec('curl https://checkip.amazonaws.com', (err, ipstdout, stderr)=> {
                 if (err) console.log("Error", err) 
