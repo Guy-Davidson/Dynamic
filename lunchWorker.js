cb08be0325dd7c978e3d2602bd365c965aded198
Guy Davidson <guydav12@gmail.com>
2022-06-03 15:30:13 +0300

    await everything

--- lunchWorker.js
+++ lunchWorker.js
@@ -2,131 +2,97 @@ const fs = require('fs')
 const AWS = require('aws-sdk');
 AWS.config.region = 'eu-west-1'
 const { exec } = require('child_process');
+const util = require('util')
+const execSync = util.promisify(require('child_process').exec)
 
 const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
-const lunchWorker = (count) => {
-    ec2.createKeyPair({ KeyName: `Cloud-Computing-${Date.now()}` }, (err, keyData) => {
-        if (err) console.log("Error", err) 
-        else {           
-            let keyname = keyData["KeyName"]
-            let keyMaterial = keyData["KeyMaterial"]                 
-            fs.writeFileSync(`${keyname}.pem`, keyMaterial, {mode: 0o400})            
+const sharedData = {}
 
-            exec('curl https://checkip.amazonaws.com', (err, ipstdout, stderr)=> {
-                if (err) console.log("Error", err) 
-                else {
-                    let myIp = ipstdout
-                    myIp = myIp.slice(0, myIp.length - 1)                    
-                    let sgName = `scriptSG-${Date.now()}`
-                    
-                    let sgParams = {
-                        GroupName: sgName,
-                        Description: "script gen sg",                                    
-                    }
+const lunchWorker = async (count) => {
+    if(!Object.keys(sharedData).length) await initSharedData()
 
-                    ec2.createSecurityGroup(sgParams, (err, sgData) => {
-                        if (err) console.log("Error", err) 
-                        else {
-                            console.log(sgData);
-                            const SecurityGroupId = sgData.GroupId;                                        
-                            const paramsIngress = {
-                                GroupId: SecurityGroupId,
-                                IpPermissions: [
-                                  {
-                                    IpProtocol: "tcp",
-                                    FromPort: 22,
-                                    ToPort: 22,                                                
-                                    IpRanges: [{ CidrIp: `${myIp}/32` }],
-                                  },
-                                  {
-                                    IpProtocol: "tcp",
-                                    FromPort: 5000,
-                                    ToPort: 5000,
-                                    IpRanges: [{ CidrIp: "0.0.0.0/0" }],
-                                  },
-                                ],
-                              };
-                            ec2.authorizeSecurityGroupIngress(paramsIngress, (err, authData) => {
-                                if (err) console.log("Error", err) 
-                                else {
-                                    console.log(authData);
-                                    const instanceParams = {
-                                        ImageId: "ami-08ca3fed11864d6bb", 
-                                        InstanceType: 't2.micro',
-                                        KeyName: keyname,
-                                        MinCount: 1,
-                                        MaxCount: 1,                                                    
-                                        SecurityGroupIds: [SecurityGroupId]
-                                    }
-                                
-                                    ec2.runInstances(instanceParams).promise()                         
-                                        .then((data) => {                                
-                                            const instanceId = data.Instances[0].InstanceId
-                                            console.log("Created instance", instanceId)
+    const { instanceParams, keyname } = sharedData
+    ec2.runInstances(instanceParams).promise()                         
+    .then((data) => {                                
+        const instanceId = data.Instances[0].InstanceId
+        console.log("Created instance", instanceId)    
+        fs.writeFileSync(`workerId-${count}.txt`, instanceId)   
+    
+        ec2.waitFor('instanceRunning', { InstanceIds: [instanceId] } , async (err, data) => {
+            if (err) console.log(err, err.stack)
+            else {                                        
+                let newWorkerIP = data["Reservations"][0]["Instances"][0]["PublicIpAddress"]            
+                console.log(newWorkerIP);
+    
+                try {
+                    await execSync(`scp -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" onWorkerScript.bash ../ips.txt workerId-${count}.txt ubuntu@${newWorkerIP}:/home/ubuntu/`)                                                                                
+                    await execSync(`ssh -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "mkdir .aws"`)                                                                                
+                    await execSync(`scp -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ../.aws/credentials ../.aws/config ubuntu@${newWorkerIP}:~/.aws/`)                                                                                
+                    await execSync(`ssh -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "sudo bash ~/onWorkerScript.bash"`)                                                                                
+                    await execSync(`ssh -T -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "cd app && pm2 start index.js"`, {shell:true})
+                    console.log(console.log("Worker Lunched successfully."));
+                } catch (err) {
+                    console.log(err);
+                }                                    
+            }   
+          });
+        
+    })
+    .catch((err) => console.error(err, err.stack))
+}
 
+const initSharedData = async () => {
+    try {
+        const keyData = await ec2.createKeyPair({ KeyName: `Cloud-Computing-${Date.now()}` }).promise()
+        let keyname = keyData["KeyName"]
+        let keyMaterial = keyData["KeyMaterial"]                 
+        fs.writeFileSync(`${keyname}.pem`, keyMaterial, {mode: 0o400}) 
 
-                                            //TODO: only the last id ends up sending to all the workers.
-
-                                            fs.writeFileSync(`workerId-${count}.txt`, instanceId)   
-            
-                                            ec2.waitFor('instanceRunning', { InstanceIds: [instanceId] } , (err, data) => {
-                                                if (err) console.log(err, err.stack)
-                                                else {                                        
-                                                    let newWorkerIP = data["Reservations"][0]["Instances"][0]["PublicIpAddress"]            
-                                                    console.log(newWorkerIP);
-                                                    
-                                                    exec(`scp -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" onWorkerScript.bash ../ips.txt workerId-${count}.txt ubuntu@${newWorkerIP}:/home/ubuntu/`, (err, stdout, stderr)=> {
-                                                        if(err) console.log(err);
-                                                        else {
-                                                            console.log(stdout);
-                                                            exec(`ssh -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "mkdir .aws"`, (err, stdout, stderr)=> {
-                                                                if(err) console.log(err);    
-                                                                else {
-                                                                    console.log(stdout);
-                                                                    exec(`scp -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ../.aws/credentials ../.aws/config ubuntu@${newWorkerIP}:~/.aws/`, (err, stdout, stderr)=> {
-                                                                        if(err) console.log(err);  
-                                                                        else {
-                                                                            console.log(stdout);
-                                                                            exec(`ssh -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "sudo bash ~/onWorkerScript.bash"`, (err, stdout, stderr)=> { 
-                                                                                if(err) console.log(err);   
-                                                                                else {
-                                                                                    console.log(stdout);
-                                                                                    console.log("Waiting for Worker to init...");                                                                        
-                                                                                    exec(`ssh -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "ls"`, {shell:true},(err, stdout, stderr)=> {
-                                                                                    if(err) console.log(err);
-                                                                                    else {                                                                                                                                                                    
-                                                                                        console.log(stdout);
-                                                                                        exec(`ssh -T -i ${keyname}.pem -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@${newWorkerIP} "cd app && pm2 start index.js"`, {shell:true},(err, stdout, stderr)=> {
-                                                                                            if(err) console.log(err);
-                                                                                            else {
-                                                                                                console.log(console.log("Worker Lunched successfully."));
-                                                                                            }
-                                                                                        })
-                                                                                    }
-                                                                                    })
-                                                                                }
-                                                                            })
-                                                                        }
-                                                                    })
-
-                                                                }
-                                                            })
-                                                        }
-                                                    })                                        
-                                                }   
-                                              });
-                                            
-                                        })
-                                        .catch((err) => console.error(err, err.stack))
-                                }
-                            })
-                        }
-                    })
-                }
-            })
+        const { stdout, stderr } = await execSync('curl https://checkip.amazonaws.com')                
+        let myIp = stdout.slice(0, stdout.length - 1)                    
+        let sgName = `scriptSG-${Date.now()}`
+        
+        let sgParams = {
+            GroupName: sgName,
+            Description: "script gen sg",                                    
         }
-     });
+
+        const sgData = await ec2.createSecurityGroup(sgParams).promise()
+        console.log(sgData)
+        const SecurityGroupId = sgData.GroupId                                       
+        const paramsIngress = {
+            GroupId: SecurityGroupId,
+            IpPermissions: [
+              {
+                IpProtocol: "tcp",
+                FromPort: 22,
+                ToPort: 22,                                                
+                IpRanges: [{ CidrIp: `${myIp}/32` }],
+              },
+              {
+                IpProtocol: "tcp",
+                FromPort: 5000,
+                ToPort: 5000,
+                IpRanges: [{ CidrIp: "0.0.0.0/0" }],
+              },
+            ],
+          }
+        
+        const authData = await ec2.authorizeSecurityGroupIngress(paramsIngress).promise()
+        console.log(authData);
+        const instanceParams = {
+            ImageId: "ami-08ca3fed11864d6bb", 
+            InstanceType: 't2.micro',
+            KeyName: keyname,
+            MinCount: 1,
+            MaxCount: 1,                                                    
+            SecurityGroupIds: [SecurityGroupId]
+        }
+        
+        sharedData["instanceParams"] = instanceParams
+        sharedData["keyname"] = keyname
+    } catch (err) { console.log(err) }
 }
 
 module.exports = lunchWorker
